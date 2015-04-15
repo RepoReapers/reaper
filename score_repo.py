774da53edef30cb2f3c45cc47c63d46f142a4e07
Craig Cabrey <craigcabrey@gmail.com>
2015-04-15 17:12:48 -0400

    Use four space indentation, repo_path to arguments

--- score_repo.py
+++ score_repo.py
@@ -3,45 +3,60 @@
 import argparse
 import importlib
 import json
+import os
 import sys
 
-def loadAttributePlugins(attributes):
-  for attribute in attributes:
-    if attribute['enabled']:
-      try:
-        attribute['implementation'] = importlib.import_module("attributes.{0}.main".format(attribute['name']))
-      except ImportError:
-        print("Failed to load the {0} attribute.".format(attribute['name']))
+def load_attribute_plugins(attributes):
+    for attribute in attributes:
+        if attribute['enabled']:
+            try:
+                attribute['implementation'] = importlib.import_module("attributes.{0}.main".format(attribute['name']))
+            except ImportError:
+                print("Failed to load the {0} attribute.".format(attribute['name']))
 
-def processConfiguration(config_file):
-  try:
-    config = json.load(config_file)
-    return config
-  except:
-    print("Malformatted or missing configuration.")
-    sys.exit(2)
+def process_configuration(config_file):
+    try:
+        config = json.load(config_file)
+        return config
+    except:
+        print("Malformatted or missing configuration.")
+        sys.exit(2)
 
-def processArguments():
-  parser = argparse.ArgumentParser(description='Calculate the score of a repository.')
-  parser.add_argument('-c', '--config', type=argparse.FileType('r'), default='config.json', dest='config_file', help='Path to the configuration file.')
-  parser.add_argument('repository_id', type=int, nargs=1, help='Identifier for a repository as it appears in the GHTorrent database.')
+def repository_path(path_string):
+    if os.path.exists(path_string):
+        if os.path.exists("{0}/.git".format(path_string)):
+            return path_string
+        else:
+            raise argparse.ArgumentTypeError("{0} is not a git repository.".format(path_string))
+    else:
+        raise argparse.ArgumentTypeError("{0} is not a directory.".format(path_string))
 
-  if len(sys.argv) is 1:
-    parser.print_help()
-    sys.exit(1)
+def process_arguments():
+    parser = argparse.ArgumentParser(description='Calculate the score of a repository.')
+    parser.add_argument('-c', '--config', type=argparse.FileType('r'), default='config.json', dest='config_file', help='Path to the configuration file.')
+    parser.add_argument('repository_id', type=int, nargs=1, help='Identifier for a repository as it appears in the GHTorrent database.')
+    parser.add_argument('repository_path', type=repository_path, nargs=1, help='Path to the repository source code.')
 
-  return parser.parse_args()
+    if len(sys.argv) is 1:
+        parser.print_help()
+        sys.exit(1)
+
+    return parser.parse_args()
 
 def main():
-  args = processArguments()
-  config = processConfiguration(args.config_file)
-  attributes = config['attributes']
-  loadAttributePlugins(attributes)
+    args = process_arguments()
+    config = process_configuration(args.config_file)
+    attributes = config['attributes']
+    load_attribute_plugins(attributes)
 
-  score = 0
-  for attribute in attributes:
-    result = attribute['implementation'].run(metadata, repo_path, attribute['options'])
-    score += result * attribute['weight']
+    score = 0
+    for attribute in attributes:
+        result = attribute['implementation'].run(config.repository_id, config.repository_path, attribute['options'])
+        score += result * attribute['weight']
 
 if __name__ == '__main__':
-  main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Caught interrupt, exiting.")
+        sys.exit(1)
