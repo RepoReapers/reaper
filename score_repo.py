9e2a5e4285e67c9bab85b193d3a2e9f9e0159420
Craig Cabrey <craigcabrey@gmail.com>
2015-04-15 17:24:13 -0400

    Connect to database through mysql.connector

--- score_repo.py
+++ score_repo.py
@@ -3,6 +3,7 @@
 import argparse
 import importlib
 import json
+import mysql.connector
 import os
 import sys
 
@@ -13,6 +14,14 @@ def load_attribute_plugins(attributes):
                 attribute['implementation'] = importlib.import_module("attributes.{0}.main".format(attribute['name']))
             except ImportError:
                 print("Failed to load the {0} attribute.".format(attribute['name']))
+
+def establish_database_connection(config):
+    try:
+        connection = mysql.connector.connect(**config)
+        return connection
+    except mysql.connector.Error:
+        print("Unable to establish connection to database.")
+        sys.exit(1)
 
 def process_configuration(config_file):
     try:
@@ -46,13 +55,17 @@ def process_arguments():
 def main():
     args = process_arguments()
     config = process_configuration(args.config_file)
+    connection = establish_database_connection(config['options']['datasource'])
     attributes = config['attributes']
     load_attribute_plugins(attributes)
 
     score = 0
     for attribute in attributes:
-        result = attribute['implementation'].run(config.repository_id, config.repository_path, attribute['options'])
+        cursor = connection.cursor()
+        result = attribute['implementation'].run(config.repository_id, config.repository_path, cursor, attribute['options'])
         score += result * attribute['weight']
+
+    connection.close()
 
 if __name__ == '__main__':
     try:
