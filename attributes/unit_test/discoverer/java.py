f8ea56d916dabae90bcf864fadd3e4a164884440
Nuthan Munaiah <nm6061@g.rit.edu>
2015-04-23 02:15:54 -0400

    Preliminary implementation of 5 languages done.

--- attributes/unit_test/discoverer/java.py
+++ attributes/unit_test/discoverer/java.py
@@ -1,6 +1,35 @@
+import utilities
 from attributes.unit_test.discoverer import TestDiscoverer
+
+LANGUAGE = 'Java'
 
 
 class JavaTestDiscoverer(TestDiscoverer):
     def __init__(self):
-        print('Java Test Discoverer')
+        self.frameworks = [
+            self.__junit__
+        ]
+
+    def __junit__(self, path):
+        proportion = -1
+
+        if utilities.search('junit', path, include=['pom.xml']):
+            files = utilities.search(
+                'import (org.junit|junit.framework)',
+                path, include=['*.java']
+            )
+
+            if files:
+                # SLOC of source code
+                sloc_code = utilities.get_loc(path)
+
+                # SLOC of test code
+                sloc_test = utilities.get_loc(path, files=files)
+
+                if LANGUAGE in sloc_code and LANGUAGE in sloc_test:
+                    proportion = (
+                        sloc_test[LANGUAGE]['sloc'] /
+                        sloc_code[LANGUAGE]['sloc']
+                    )
+
+        return proportion
