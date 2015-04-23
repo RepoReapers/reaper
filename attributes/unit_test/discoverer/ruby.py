9457ed541356433e5a3c7b3e5c5701b1e1d682c0
Nuthan Munaiah <nm6061@rit.edu>
2015-04-23 02:15:54 -0400

    Preliminary implementation of 5 languages done.

--- attributes/unit_test/discoverer/ruby.py
+++ attributes/unit_test/discoverer/ruby.py
@@ -1,6 +1,35 @@
+import utilities
 from attributes.unit_test.discoverer import TestDiscoverer
+
+LANGUAGE = 'Ruby'
 
 
 class RubyTestDiscoverer(TestDiscoverer):
     def __init__(self):
-        print('Ruby Test Discoverer')
+        self.frameworks = [
+            self.__rspec__
+        ]
+
+    def __rspec__(self, path):
+        proportion = -1
+
+        if utilities.search('rspec', path, whole=True):
+            files = utilities.search(
+                '(describe)(.*)(do)',
+                path, include=['*.rb']
+            )
+
+            if files:
+                # SLOC of source code
+                sloc_code = utilities.get_loc(path)
+
+                # SLOC of test code
+                sloc_test = utilities.get_loc(path, files=files)
+
+                if LANGUAGE in sloc_code and LANGUAGE in sloc_test:
+                    proportion = (
+                        sloc_test[LANGUAGE]['sloc'] /
+                        sloc_code[LANGUAGE]['sloc']
+                    )
+
+        return proportion
