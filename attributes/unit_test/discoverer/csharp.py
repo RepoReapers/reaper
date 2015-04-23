b07a8cfe0db61629d094b84bf84f30ed7a4e9a84
Nuthan Munaiah <nm6061@rit.edu>
2015-04-23 13:02:38 -0400

    C# NUnit discovery done

--- attributes/unit_test/discoverer/csharp.py
+++ attributes/unit_test/discoverer/csharp.py
@@ -1,6 +1,34 @@
+import utilities
 from attributes.unit_test.discoverer import TestDiscoverer
+
+LANGUAGE = 'C#'
 
 
 class CSharpTestDiscoverer(TestDiscoverer):
     def __init__(self):
-        print('C# Test Discoverer')
+        self.frameworks = [
+            self.__nunit__
+        ]
+
+    def __nunit__(self, path):
+        proportion = -1
+
+        files = utilities.search(
+            'using NUnit.Framework;',
+            path, include=['*.cs']
+        )
+
+        if files:
+            # SLOC of source code
+            sloc_code = utilities.get_loc(path)
+
+            # SLOC of test code
+            sloc_test = utilities.get_loc(path, files=files)
+
+            if LANGUAGE in sloc_code and LANGUAGE in sloc_test:
+                proportion = (
+                    sloc_test[LANGUAGE]['sloc'] /
+                    sloc_code[LANGUAGE]['sloc']
+                )
+
+        return proportion
