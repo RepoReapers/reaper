d902045e991cc778dabe31e34a6dcd119e19ccd0
Craig Cabrey <craigcabrey@gmail.com>
2015-04-26 13:10:29 -0400

    Update license attribute to return binary and raw result

--- attributes/license/main.py
+++ attributes/license/main.py
@@ -1,3 +1,4 @@
+from core import tokenize
 from utilities import url_to_json
 
 
@@ -6,18 +7,14 @@ def run(project_id, repo_path, cursor, **options):
     cursor.execute(query)
     record = cursor.fetchone()
 
-    full_url = record[0].rstrip()
+    full_url = tokenize(record[0].rstrip())
     json_response = url_to_json(full_url, headers={
             'Accept': 'application/vnd.github.drax-preview+json'
         }
     )
 
-    if 'license' in json_response:
-        result = 1
-    else:
-        result = 0
-
-    return result
+    result = 'license' in json_response
+    return result, int(result)
 
 if __name__ == '__main__':
     print("Attribute plugins are not meant to be executed directly.")
