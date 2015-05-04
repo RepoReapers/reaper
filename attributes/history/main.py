631d0d13c5b6daf0f652c17aa72af478c308cce2
njp6833 <njp6833@g.rit.edu>
2015-05-03 23:43:07 -0400

    Core algorithm for commit history
    Algorithm to calculate the average of total number of commits per week,
    comparing it with the threshold value and returning true or false based
    on that value.

--- attributes/history/main.py
+++ attributes/history/main.py
@@ -1,9 +1,28 @@
 import sys
+import json
+import mysql.connector
+import os
+import urllib
 
 
 def run(project_id, repo_path, cursor, **options):
-    return False, 0
+    query = "SELECT count(id) as total, DATEDIFF(max(created_at), min(created_at)) as Days from commits where project_id={0} and created_at>0;".format(id)
+    cursor.execute(query)
+    result = cursor.fetchone()
+	d1=float(result[0]) #total number of commits
+	d2=float(result[1]) #total number of days	
+    week=d2/7           #calculate total number of weeks
+	if week < 1:
+        return False,ans
+	else:
+	    ans = round(d1/week)
+        if ans < 2:
+        	return False,ans
+        else: 
+        	return True,ans	
+	    
+    
+
 
 if __name__ == '__main__':
-    print()
-    sys.exit(1)
+    print("Attribute plugins are not meant to be executed directly.") 
