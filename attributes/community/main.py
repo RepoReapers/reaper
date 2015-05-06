71854eaea8490374f81dc048b6726eed43f2fbcc
njp6833 <njp6833@g.rit.edu>
2015-05-06 01:46:02 -0400

    Modified cursor name in the file

--- attributes/community/main.py
+++ attributes/community/main.py
@@ -11,12 +11,12 @@ def run(project_id, repo_path, cursor, **options):
     cursor2 = connection.cursor(buffered = True)    
     #Query 1
     query1 = "select count(f.follower_id) from followers f join project_members p on f.user_id = p.user_id and p.repo_id={0} group by p.repo_id;".format(id)
-    cursor.execute(query1)
-    result1 = cursor.fetchone()
+    cursor1.execute(query1)
+    result1 = cursor1.fetchone()
     #Query 2
     query2 = "select count(user_id) from watchers where repo_id={0} group by repo_id;".format(id)
-    cur2.execute(query2)
-    result2 = cur2.fetchone()
+    cursor2.execute(query2)
+    result2 = cursor2.fetchone()
     if result1 is None or result2 is None:
         return False
     elif int(result1[0]) > 5 and int(result2[0]) > 1:
