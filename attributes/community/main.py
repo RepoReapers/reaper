2ad49a4a04bae80c0d94c293a2c0ba791adb10b3
njp6833 <njp6833@g.rit.edu>
2015-05-06 01:38:03 -0400

    Algorithm to count number of watchers and followers
    Counts number of watchers and followers for a particular repo and
    return true or false.

--- attributes/community/main.py
+++ attributes/community/main.py
@@ -1,9 +1,39 @@
 import sys
+import json
+import mysql.connector
+import os
+import urllib
 
 
 def run(project_id, repo_path, cursor, **options):
-    return False, 0
+	id = project_id
+    cursor1 = connection.cursor(buffered = True)
+    cursor2 = connection.cursor(buffered = True)    
+    #Query 1
+	query1 = "select count(f.follower_id) from followers f join project_members p on f.user_id = p.user_id and p.repo_id={0} group by p.repo_id;".format(id)
+    cursor.execute(query1)
+    result1 = cursor.fetchone()
+    #Query 2
+    query2 = "select count(user_id) from watchers where repo_id={0} group by repo_id;".format(id)
+    cur2.execute(query2)
+    result2 = cur2.fetchone()
+
+    if result1 is None or result2 is None:
+        return False
+    elif int(result1[0]) > 5 and int(result2[0]) > 1:
+        return True
+    else:
+        return False
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    print()
-    sys.exit(1)
+    print('Attribute plugins are not meant to be executed directly.')
+    
