c04cf2fa304463868dfc89f8ad33641298021b32
Nuthan Munaiah <nm6061@g.rit.edu>
2015-05-13 18:25:21 -0400

    Processing repositories in parallel

--- batch_score.py
+++ batch_score.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 import argparse
+import copy
+import json
+import os
+import sys
+import time
+import traceback
+from multiprocessing import Pool
+
 from core import load_attribute_plugins
 from core import global_init_attribute_plugins
 from core import init_attribute_plugins
@@ -10,11 +18,7 @@ from core import process_plugins
 from core import process_key_string
 from core import process_repository
 from core import get_persist_attrs
-import json
-import time
-import os
-import sys
-from utilities import is_dir
+from utilities import is_dir, get_repo_path
 
 
 def process_arguments():
@@ -66,6 +70,18 @@ def process_arguments():
         required=False,
         help='String of attribute initials. Uppercase to persist data'
     )
+    parser.add_argument(
+        '-n',
+        '--num-processes',
+        type=int,
+        dest='num_processes',
+        default=1,
+        required=False,
+        help=(
+            'Number of processes to spawn when processing repositories'
+            ' from the samples file.'
+        )
+    )
 
     if len(sys.argv) < 2:
         parser.print_help()
@@ -73,8 +89,9 @@ def process_arguments():
 
     return parser.parse_args()
 
-def digits_in(num):
-    return len(str(num))
+
+def digits_in(num): return len(str(num))
+
 
 def main():
     """
@@ -83,41 +100,59 @@ def main():
     args = process_arguments()
 
     sample = [int(line) for line in args.repositories_sample]
-    digits = digits_in(len(sample)) # digits in the size of the sample
+    # Digits in the size of the sample
+    digits = digits_in(len(sample))
 
     config = process_configuration(args.config_file)
+    db_settings = config['options']['datasource']
     (threshold, attributes) = process_plugins(args.plugins_dir)
 
     # If a keystring exists, then this function will overwrite enabled/persist
     # states based on the keystring
     process_key_string(attributes, args.key_string)
+    attributes_copy = copy.deepcopy(attributes)
 
     (reporting, persist_attrs) = get_persist_attrs(attributes)
     report = []
 
-    connection = establish_database_connection(config['options']['datasource'])
+    connection = establish_database_connection(db_settings)
 
+    # Do before any multiprocessing
     load_attribute_plugins(args.plugins_dir, attributes)
 
-    # Do before any multiprocessing    
+    # Do before any multiprocessing
     global_init_attribute_plugins(attributes, connection, sample)
+    connection.close()
 
-    # Do within child processes to initialize global data structures
-    init_attribute_plugins(attributes, connection)
+    with Pool(args.num_processes) as pool:
+        report = pool.starmap(
+            process,
+            [(
+                repo_id,
+                get_repo_path(repo_id, args.repositories_dir),
+                db_settings,
+                attributes_copy,
+                args.plugins_dir,
+                persist_attrs,
+                threshold
+            ) for repo_id in sample]
+        )
 
-    left = len(sample)
-    for repo_id in sample:
-        # Repository path is the subdir within the id folder whose name is
-        # NOT metadata.json. That is, the path we're interested in is the
-        # downloaded git source. If no such path exists, the repository path
-        # is simply the path to the id folder.
-        base_path = args.repositories_dir + str(repo_id) + "/"
-        repo_path = base_path  # Default to this if we can't find the git repo
-        for entry in os.listdir(base_path):
-            if entry != 'metadata.json':
-                repo_path += entry
-                break
+    if reporting:
+        report_name = "report_" + str(int(time.time())) + ".json"
+        with open(report_name, 'w') as report_file:
+            json.dump(report, report_file)
 
+
+def process(repo_id, repo_path, db_settings, attributes, plugins_dir,
+            persist_attrs, threshold):
+    repo_result = {"id": repo_id}
+
+    try:
+        connection = establish_database_connection(db_settings)
+
+        load_attribute_plugins(plugins_dir, attributes)
+        init_attribute_plugins(attributes, connection)
         score, results = process_repository(
             repo_id,
             repo_path,
@@ -125,34 +160,26 @@ def main():
             connection
         )
 
-        if reporting:
-            report_entry = {"id": repo_id}
-            for attr, result in results.items():
-                if attr in persist_attrs:
-                    report_entry[attr] = float(result)
-            report.append(report_entry)
-
         # Generate a green checkmark or red x using terminal escapes
         if score > threshold:
             result_char = '\033[92m✓\033[0m'
         else:
             result_char = '\033[91m✘\033[0m'
 
-        # When we print the next status line, we will have finished one repo
-        left -= 1
+        for attr, result in results.items():
+            if attr in persist_attrs:
+                repo_result[attr] = float(result)
+    except:
+        sys.stderr.write('Exception\n\n')
+        sys.stderr.write('Project:\n')
+        sys.stderr.write('  ID   {0}\n'.format(repo_id))
+        sys.stderr.write('  Path {0}\n\n'.format(repo_path))
+        extype, exvalue, extrace = sys.exc_info()
+        traceback.print_exception(extype, exvalue, extrace)
+    finally:
+        connection.close()
 
-        # Number of repos left to process out of the total number
-        cur = ('{:0>' + str(digits) + 'd}').format(left)
-        tot = str(len(sample))
-        sid = '{: >11d}'.format(repo_id)
-
-        print('remaining [{cur} / {tot}] ght_id [{sid}] {res}'.format(
-            cur=cur, tot=tot, sid=sid, res=result_char))
-
-    if reporting:
-        report_name = "report_" + str(int(time.time())) + ".json"
-        with open(report_name, 'w') as report_file:
-            json.dump(report, report_file)
+    return repo_result
 
 if __name__ == '__main__':
     main()
