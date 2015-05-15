56c14d6077b5079642cc7103b47e2b8b4b086cf8
Nuthan Munaiah <nm6061@g.rit.edu>
2015-05-15 19:12:42 -0400

    Returning {} instead of () as the raw result

--- attributes/community/main.py
+++ attributes/community/main.py
@@ -1,12 +1,14 @@
 import sys
-import json
-import mysql.connector
-import os
-import urllib
+
+from core import Tokenizer
+from utilities import url_to_jsonimport urllib
 
 
 def run(project_id, repo_path, cursor, **options):
-    id = project_id
+    t_sub = options.get('sub')
+    t_star = options.get('star')
+    t_forks = options.get('forks')
+
     cursor.execute('''
         SELECT
             url
@@ -16,26 +18,34 @@ def run(project_id, repo_path, cursor, **options):
             id = {0}
         '''.format(project_id))
 
-    record = cursor.fetchone()	
+    record = cursor.fetchone()
+
     tokenizer = Tokenizer()
     full_url = tokenizer.tokenize(record[0].rstrip())
-    json_response = url_to_json(full_url)	
-    subscribers_count=json_response['subscribers_count']	
-    stargazers_count=json_response['stargazers_count']
-    forks=json_response['forks']
-    sub = options.get('sub')
-    star = options.get('star')
-    forks = options.get('forks')			
-    if (subscribers_count >= sub and stargazers_count >= star) or (stargazers_count >= star and forks >= forks) or (subscribers_count >= sub and forks >= forks):
-        return (True,(subscribers_count,stargazers_count,forks))
-    else: 					
-        return (False,(subscribers_count,stargazers_count,forks))
-    				
-    	
+    json_response = url_to_json(full_url)
+
+    subscribers_count = json_response['subscribers_count']
+    stargazers_count = json_response['stargazers_count']
+    forks = json_response['forks']
+
+    result = False
+    if (
+            (subscribers_count >= t_sub and stargazers_count >= t_star) or
+            (stargazers_count >= t_star and forks >= t_forks) or
+            (subscribers_count >= t_sub and forks >= t_forks)
+       ):
+        result = True
+
+    return (
+        result,
+        {
+            'sub': subscribers_count,
+            'star': stargazers_count,
+            'forks': forks
+        }
+    )
 
 
 if __name__ == '__main__':
     print('Attribute plugins are not meant to be executed directly.')
-else:
-    from core import Tokenizer
-    from utilities import url_to_json    
+    sys.exit(1)
