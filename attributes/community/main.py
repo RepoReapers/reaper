ca17b2c196c8720b6be14b84c0df6a4724e0e85e
njp6833 <njp6833@g.rit.edu>
2015-05-15 16:11:31 -0400

    working copy for community

--- attributes/community/main.py
+++ attributes/community/main.py
@@ -7,18 +7,35 @@ import urllib
 
 def run(project_id, repo_path, cursor, **options):
     id = project_id
-    cursor = connection.cursor()
-    query = "select count(user_id) from watchers where repo_id={0} group by repo_id;".format(id)
-    cursor.execute(query)
-    result = cursor.fetchone()
-    if result is None:
-        return False
-    elif int(result[0]) > 1:
-        return True
-    else:
-        return False
+    cursor.execute('''
+        SELECT
+            url
+        FROM
+            projects
+        WHERE
+            id = {0}
+        '''.format(project_id))
+
+    record = cursor.fetchone()	
+    tokenizer = Tokenizer()
+    full_url = tokenizer.tokenize(record[0].rstrip())
+    json_response = url_to_json(full_url)	
+    subscribers_count=json_response['subscribers_count']	
+    stargazers_count=json_response['stargazers_count']
+    forks=json_response['forks']
+    sub = options.get('sub')
+    star = options.get('star')
+    forks = options.get('forks')			
+    #if (subscribers_count >= sub and stargazers_count >= star) or (stargazers_count >= star and forks >= forks) or (subscribers_count >= sub and forks >= forks):
+        #return True,(subscribers_count,stargazers_count,forks)
+    #else: 					
+    return (False,(subscribers_count,stargazers_count,forks))
+    				
+    	
 
 
 if __name__ == '__main__':
     print('Attribute plugins are not meant to be executed directly.')
-    
+else:
+    from core import Tokenizer
+    from utilities import url_to_json    
