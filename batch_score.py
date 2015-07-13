62eb17ea05537215b0bb2699ee8dc153a01458a2
Nuthan Munaiah <nm6061@g.rit.edu>
2015-07-12 23:15:45 -0400

    Refactored to use Run, Database, and Attributes

--- batch_score.py
+++ batch_score.py
@@ -1,28 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
-import copy
-import json
-import multiprocessing
-import numbers
-import multiprocessing
-import os
 import sys
-import time
 import traceback
 
-from lib.core import load_attribute_plugins
-from lib.core import global_init_attribute_plugins
-from lib.core import init_attribute_plugins
-from lib.core import establish_database_connection
-from lib.core import process_configuration
-from lib.core import process_plugins
-from lib.core import process_key_string
-from lib.core import process_repository
-from lib.core import get_persist_attrs
-from lib.core import get_run_id
-from lib.core import save_result
-from lib.utilities import is_dir, get_repo_path
+from lib import core, utilities, run
+from lib.attributes import Attributes
+from lib.database import Database
 
 
 def process_arguments():
@@ -44,19 +28,19 @@ def process_arguments():
         help='Path to the configuration file.'
     )
     parser.add_argument(
-        '-p',
-        '--plugins',
-        type=is_dir,
-        default='attributes',
-        dest='plugins_dir',
-        help='Path to the folder containing your set of plugins'
+        '-m',
+        '--manifest',
+        type=argparse.FileType('r'),
+        default='manifest.json',
+        dest='manifest_file',
+        help='Path to the manifest file.'
     )
     parser.add_argument(
-        '-d',
-        '--repositories-dir',
-        type=is_dir,
-        dest='repositories_dir',
-        help='Path to the collection of downloaded repositories.'
+        '-r',
+        '--repositories-root',
+        type=utilities.is_dir,
+        dest='repositories_root',
+        help='Path to the root of downloaded repositories.'
     )
     parser.add_argument(
         '-s',
@@ -98,109 +82,31 @@ def main():
     """
     Main execution flow.
     """
-    args = process_arguments()
-
-    sample = [int(line) for line in args.repositories_sample]
-
-    config = process_configuration(args.config_file)
-    db_settings = config['options']['datasource']
-    (threshold, attributes) = process_plugins(args.plugins_dir)
-
-    # If a keystring exists, then this function will overwrite enabled/persist
-    # states based on the keystring
-    process_key_string(attributes, args.key_string)
-    attributes_copy = copy.deepcopy(attributes)
-
-    (reporting, persist_attrs) = get_persist_attrs(attributes)
-    report = []
-
-    connection = establish_database_connection(db_settings)
-
-    # Do before any multiprocessing
-    load_attribute_plugins(args.plugins_dir, attributes)
-
-    # Do before any multiprocessing
-    global_init_attribute_plugins(attributes, connection, sample)
-
-    # Generate a run_id with which processes are to save results
-    cursor = connection.cursor()
-    run_id = get_run_id(cursor)
-    cursor.close()
-
-    run_str = 'Beginning execution with id {0}'.format(run_id)
-    print('{0}\n{1}\n{0}'.format('=' * len(run_str), run_str))
-
-    connection.close()
-
-    with multiprocessing.Pool(args.num_processes) as pool:
-        report = pool.starmap(
-            process,
-            [(
-                run_id,
-                repo_id,
-                get_repo_path(repo_id, args.repositories_dir),
-                db_settings,
-                attributes_copy,
-                args.plugins_dir,
-                persist_attrs,
-                threshold
-            ) for repo_id in sample]
-        )
-
-    if reporting:
-        report_name = "report_" + str(int(time.time())) + ".json"
-        with open(report_name, 'w') as report_file:
-            json.dump(report, report_file)
-
-
-def process(run_id, repo_id, repo_path, db_settings, attributes, plugins_dir,
-            persist_attrs, threshold):
-    repo_result = {"id": repo_id}
-    score = None
-
     try:
-        connection = establish_database_connection(db_settings)
+        args = process_arguments()
 
-        load_attribute_plugins(plugins_dir, attributes)
-        init_attribute_plugins(attributes, connection)
+        config = utilities.read(args.config_file)
+        manifest = utilities.read(args.manifest_file)
 
-        score, results = process_repository(
-            repo_id,
-            repo_path,
-            attributes,
-            connection
+        # TODO: Refactor
+        core.config = config
+
+        database = Database(config['options']['datasource'])
+        attributes = Attributes(
+            manifest['attributes'], database, args.key_string
         )
 
-        # Generate a green checkmark or red x using terminal escapes
-        if score >= threshold:
-            result_char = '\033[92m✓\033[0m'
-        else:
-            result_char = '\033[91m✘\033[0m'
-
-        print('[{0:>10s}] {1}: {2}'.format(str(repo_id), result_char, score))
-
-        for attr, result in results.items():
-            if attr in persist_attrs:
-                repo_result[attr] = result
-    except:
-        sys.stderr.write('Exception\n\n')
-        sys.stderr.write('Project:\n')
-        sys.stderr.write('  ID   {0}\n'.format(repo_id))
-        sys.stderr.write('  Path {0}\n\n'.format(repo_path))
+        _run = run.Run(
+            args.repositories_root, attributes, database,
+            config['options']['threshold'], args.num_processes
+        )
+        _run.run([int(line) for line in args.repositories_sample])
+    except Exception as e:
         extype, exvalue, extrace = sys.exc_info()
         traceback.print_exception(extype, exvalue, extrace)
-    finally:
-        cursor = connection.cursor()
-        save_result(run_id, repo_id, repo_result, score, cursor)
-        cursor.close()
-        connection.close()
-
-    return repo_result
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print(
-            '\rCaught interrupt, killing all children...'
-        )
+        print('\rCaught interrupt, killing all children...')
