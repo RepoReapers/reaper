a8e8006890da39edabb995be827cb5ae54e92bed
Nuthan Munaiah <nm6061@g.rit.edu>
2015-07-13 00:17:55 -0400

    Skip test if repositories are not present

--- tests/unit_test/test_main.py
+++ tests/unit_test/test_main.py
@@ -6,6 +6,7 @@ from tests import REPOS_PATH
 
 
 class MainTestCase(unittest.TestCase):
+    @unittest.skipIf(not os.path.exists(REPOS_PATH), 'setup.sh not run.')
     def test_main(self):
         # Test: Project using Mocha
         (result, value) = main.run(
