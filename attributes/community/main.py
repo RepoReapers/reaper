f4fea615d84e07bc58c27968f8024440c061c833
Nuthan Munaiah <nm6061@g.rit.edu>
2015-07-22 02:00:27 -0400

    Community now represents total number of authors
    This resolves #4

--- attributes/community/main.py
+++ attributes/community/main.py
@@ -4,46 +4,22 @@ from lib.core import Tokenizer
 from lib.utilities import url_to_json
 
 
+# Query counts the number of distinct authors contributing to a project.
+QUERY = '''
+    SELECT COUNT(DISTINCT(c.author_id))
+    FROM commits c JOIN project_commits pc
+      ON pc.commit_id = c.id
+    WHERE pc.project_id = {0}
+'''
+
+
 def run(project_id, repo_path, cursor, **options):
-    t_sub = options.get('sub')
-    t_star = options.get('star')
-    t_forks = options.get('forks')
-
-    cursor.execute('''
-        SELECT
-            url
-        FROM
-            projects
-        WHERE
-            id = {0}
-        '''.format(project_id))
-
+    cursor.execute(QUERY.format(project_id))
     record = cursor.fetchone()
+    num_authors = record[0]
+    threshold = options.get('threshold', 1)
 
-    tokenizer = Tokenizer()
-    full_url = tokenizer.tokenize(record[0].rstrip())
-    json_response = url_to_json(full_url)
-
-    subscribers_count = json_response.get('subscribers_count', 0)
-    stargazers_count = json_response.get('stargazers_count', 0)
-    forks = json_response.get('forks', 0)
-
-    result = False
-    if (
-            (subscribers_count >= t_sub and stargazers_count >= t_star) or
-            (stargazers_count >= t_star and forks >= t_forks) or
-            (subscribers_count >= t_sub and forks >= t_forks)
-       ):
-        result = True
-
-    return (
-        result,
-        {
-            'sub': subscribers_count,
-            'star': stargazers_count,
-            'forks': forks
-        }
-    )
+    return (int(num_authors >= threshold), num_authors)
 
 
 if __name__ == '__main__':
