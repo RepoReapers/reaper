c2fbfd09becb4b0b7cb0bb0e24caba241d55f9a9
Nuthan Munaiah <nm6061@g.rit.edu>
2015-07-24 17:24:23 -0400

    Metric represents the number of core contributors
    Change resolves #4

--- attributes/community/main.py
+++ attributes/community/main.py
@@ -6,20 +6,34 @@ from lib.utilities import url_to_json
 
 # Query counts the number of distinct authors contributing to a project.
 QUERY = '''
-    SELECT COUNT(DISTINCT(c.author_id))
+    SELECT c.author_id, COUNT(*)
     FROM commits c JOIN project_commits pc
       ON pc.commit_id = c.id
     WHERE pc.project_id = {0}
+    GROUP BY c.author_id
+    ORDER BY COUNT(*) DESC
 '''
 
 
 def run(project_id, repo_path, cursor, **options):
-    cursor.execute(QUERY.format(project_id))
-    record = cursor.fetchone()
-    num_authors = record[0]
-    threshold = options.get('threshold', 1)
+    num_core_contributors = 0
 
-    return (int(num_authors >= threshold), num_authors)
+    commits = dict()
+    cursor.execute(QUERY.format(project_id))
+    for row in cursor:
+        commits[row[0]] = row[1]
+    num_commits = sum(commits.values())
+
+    cutoff = options.get('cutoff', 1.0)
+    aggregate = 0
+    for (_, v) in commits.items():
+        num_core_contributors += 1
+        aggregate += v
+        if (aggregate / num_commits) >= cutoff:
+            break
+
+    threshold = options.get('threshold', 1)
+    return (num_core_contributors >= threshold, num_core_contributors)
 
 
 if __name__ == '__main__':
