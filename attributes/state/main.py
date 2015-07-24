030d2c54b1ba775023da96a2e82d9e59a49086f9
Nuthan Munaiah <nm6061@g.rit.edu>
2015-07-24 00:00:42 -0400

    Introduced new attribute - state
    The state attribute, in its current form takes
    one of two values - active or dormant. A project
    is said to be active if the last commit made to
    the project is within a specified threshold.
    
    The duration since last commit is computed
    relative to the current date i.e. datetime.today()
    or a configured value allowing for the
    specification of the date the GHTorrent dump was
    downloaded. MAX(commits.created_at) could have
    been used but the current MAX(commits.created_at)
    has 2038 as its year.
    
    Change resolves #8

---
+++ attributes/state/main.py
@@ -0,0 +1,34 @@
+import sys
+from datetime import datetime
+
+from lib import dateutil
+from lib import utilities
+
+
+def run(project_id, repo_path, cursor, **options):
+    cursor.execute(
+        '''
+            SELECT MAX(c.created_at)
+            FROM commits c
+                JOIN project_commits pc ON pc.commit_id = c.id
+            WHERE pc.project_id = {0} and c.created_at > 0
+        '''.format(project_id)
+    )
+
+    result = cursor.fetchone()
+    last_commit_date = result[0]
+
+    # Compute the number of months between the last commit and today
+    # Note: today may be the date the GHTorrent dump was published by
+    #       ghtorrent.org
+    today = options.get('today', datetime.today().date())
+    if isinstance(today, str):
+        today = datetime.strptime(today, '%Y-%m-%d')
+    delta = dateutil.relativedelta(today, last_commit_date)
+
+    threshold = utilities.parse_datetime_delta(options.get('threshold', ''))
+    return 'active' if delta <= threshold else 'dormant', delta
+
+if __name__ == '__main__':
+    print('Attribute plugins are not meant to be executed directly.')
+    sys.exit(1)
---
+++ tests/state/test_main.py
@@ -0,0 +1,62 @@
+import json
+import os
+import unittest
+
+from attributes.state import main
+from lib import database, dateutil, utilities
+
+
+class MainTestCase(unittest.TestCase):
+    def setUp(self):
+        path = (
+            os.path.join(
+                os.path.abspath(
+                    os.path.join(
+                        os.path.dirname(os.path.realpath(__file__)),
+                        os.pardir,
+                        os.pardir
+                    )
+                ),
+                'config.json'
+            )
+        )
+        settings = None
+        with open(path, 'r') as file_:
+            settings = json.load(file_)['options']['datasource']
+
+        self.database = database.Database(settings)
+
+    def test_main(self):
+        # Arrange
+        project_id = 284
+        options = {'threshold': '6m'}
+        expected = 'active'
+
+        # Act
+        try:
+            self.database.connect()
+            with self.database.cursor() as cursor:
+                (actual, value) = main.run(project_id, '', cursor, **options)
+        finally:
+            self.database.disconnect()
+
+        # Assert
+        self.assertIsInstance(value, dateutil.relativedelta)
+        self.assertEqual(expected, actual)
+
+        # Arrange
+        project_id = 66
+        options = {'threshold': '6m'}
+        expected = 'dormant'
+
+        # Act
+        try:
+            self.database.connect()
+            with self.database.cursor() as cursor:
+                (actual, value) = main.run(project_id, '', cursor, **options)
+        finally:
+            self.database.disconnect()
+
+        # Assert
+        self.assertIsInstance(value, dateutil.relativedelta)
+        self.assertEqual(expected, actual)
