5fbfbcc2815b11f7d073d5c7bb42df8f94165f12
Nuthan Munaiah <nm6061@g.rit.edu>
2015-07-29 23:56:01 -0400

    Refactored test_save to specify SQL column names

--- tests/test_run.py
+++ tests/test_run.py
@@ -58,7 +58,8 @@ class RunTestCase(unittest.TestCase):
             rresults = {
                 'architecture': 9.9, 'continuous_integration': True,
                 'community': 9, 'documentation': 9.9, 'history': 9.9,
-                'license': True, 'management': 9.9, 'unit_test': 9.9
+                'license': True, 'management': 9.9, 'unit_test': 9.9,
+                'state': 'active'
             }
             run = Run(
                 directory, self.attributes, self.database, self.threshold,
@@ -72,20 +73,25 @@ class RunTestCase(unittest.TestCase):
             try:
                 self.database.connect()
                 actual = self.database.get(
-                    'SELECT * FROM reaper_results WHERE run_id = {0}'.format(
-                        run.run_id
-                    )
+                    '''
+                        SELECT project_id, architecture,
+                            continuous_integration, community, documentation,
+                            history, license, management, unit_test, state,
+                            score
+                        FROM reaper_results WHERE run_id = {0}
+                    '''.format(run.run_id)
                 )
-                self.assertEqual(10868464, actual[2])
-                self.assertEqual(9.9, actual[3])
-                self.assertEqual(True, actual[4])
-                self.assertEqual(9, actual[5])
-                self.assertEqual(9.9, actual[6])
+                self.assertEqual(10868464, actual[0])
+                self.assertEqual(9.9, actual[1])
+                self.assertEqual(True, actual[2])
+                self.assertEqual(9, actual[3])
+                self.assertEqual(9.9, actual[4])
+                self.assertEqual(9.9, actual[5])
+                self.assertEqual(True, actual[6])
                 self.assertEqual(9.9, actual[7])
-                self.assertEqual(True, actual[8])
-                self.assertEqual(9.9, actual[9])
-                self.assertEqual(9.9, actual[10])
-                self.assertEqual(99.99, actual[12])
+                self.assertEqual(9.9, actual[8])
+                self.assertEqual('active', actual[9])
+                self.assertEqual(99.99, actual[10])
             finally:
                 self.database.post(
                     'DELETE FROM reaper_runs WHERE id = {0}'.format(run.run_id)
