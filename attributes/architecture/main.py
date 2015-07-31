db661f440b90cd7e49e8045454ef8c3b87df5b11
Nuthan Munaiah <nm6061@g.rit.edu>
2015-07-30 22:17:48 -0400

    Revert "Symbol references must be tokens of ..."
    This reverts commit b458f5c9a2e82e688636043b2c144af9abfebda6.

--- attributes/architecture/main.py
+++ attributes/architecture/main.py
@@ -121,7 +121,7 @@ def build_graph(file_paths, graph, lexer):
                 symbol = item[1]
                 if token_type in [token.Name.Function, token.Name.Class]:
                     node.defines.add(symbol)
-                elif token_type is token.Name:
+                else:
                     node.references.add(symbol)
             if 'DEBUG' in os.environ:
                 print(node)
